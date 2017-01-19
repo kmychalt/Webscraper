af29c12ca186ced173ae315486634c0892ccbe7e
Kristin Thomas <kmychalt@gmail.com>
2017-01-19 02:01:36 -0800

    fixed hanging extensions
    now fixes enxtensions to working site name

--- scraper.py
+++ scraper.py
@@ -1,26 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
-from graphviz import Digraph
+from graphviz import Graph
         
 def startScrapin(sitename):
-    g1 = Digraph(comment = 'Scraper Results')
+    g1 = Graph(comment = 'Scraper Results')
     linklist = findTheLinks(sitename, g1)
     linklist = linklist.split()
-    #print (linklist)
     buildBranches(sitename, linklist, g1)
-    clarifyExtensions(linklist, sitename)
-    print (linklist)
-    for link in linklist:
-        sublinklist = findTheLinks(link, g1)
-        
+    #for link in linklist:
+        #print (link)
+        #sublinklist = findTheLinks(link, g1)
+        #print (sublinklist)
         #if sublinklist: #check if list is empty
             #buildBranches(link, sublinklist, g1)
-    #createImage(g1)
+    createImage(g1)
     
-def clarifyExtensions(linklist, sitename):
-    for link in linklist:
-        if (link[0] == '/'):
-            link = sitename + link   
+def clarifyExtension(link, sitename):
+    if (link[0] == '/'):
+        link = 'http://' + sitename + link
+    return link
     
 def findTheLinks(sitename, g1):
     linklist = ''
@@ -29,7 +27,8 @@ def findTheLinks(sitename, g1):
         data = page.text
         soup = BeautifulSoup(data, "lxml")
         for link in soup.find_all('a'):
-            linklist = linklist + link.get('href') + (' ')
+            link = clarifyExtension(link.get('href'), sitename)
+            linklist = linklist + link + (' ')
         return linklist
     except requests.exceptions.ConnectionError:
         requests.status_code = "Connection refused"
@@ -38,15 +37,11 @@ def findTheLinks(sitename, g1):
         print (err)
      
     
-def buildBranches(branch, linklist, g1):
-    #print ('Branch')
-    #print (branch)
-    #print ('Linklist')
-    #print (linklist)
-    g1.node(branch)
+def buildBranches(sitename, linklist, g1):
+    g1.node(sitename)
     for link in linklist:
         g1.node(link)
-        g1.edge(branch, link)
+        g1.edge(sitename, link)
     #print (g1.source)
     
 def createImage(g1):
